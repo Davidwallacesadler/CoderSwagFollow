344bafb17d26cb9fda4be31d54e05b178db5cc77
David Sadler <davidwallacesadler@gmail.com>
2019-01-12 10:54:41 -0700

    Initial commit

---
+++ CoderSwag/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CoderSwag
+//
+//  Created by David Sadler on 1/12/19.
+//  Copyright © 2019 David Sadler. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
