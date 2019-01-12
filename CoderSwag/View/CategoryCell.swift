18a0e272f119c174009e8f13afaa0cfa28f56fe9
David Sadler <davidwallacesadler@gmail.com>
2019-01-12 11:57:14 -0700

    Create navigation controller, UITableView, and UITableViewCell in interface builder

---
+++ CoderSwag/View/CategoryCell.swift
@@ -0,0 +1,27 @@
+//
+//  CategoryCell.swift
+//  CoderSwag
+//
+//  Created by David Sadler on 1/12/19.
+//  Copyright © 2019 David Sadler. All rights reserved.
+//
+
+import UIKit
+
+class CategoryCell: UITableViewCell {
+    
+    @IBOutlet weak var categoryImage: UIImageView!
+    @IBOutlet weak var categoryTitle: UILabel!
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
