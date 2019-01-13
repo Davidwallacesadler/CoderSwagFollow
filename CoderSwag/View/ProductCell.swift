69f869207300cbdfd56aa73dafce1c2e953fa60c
David Sadler <davidwallacesadler@gmail.com>
2019-01-13 15:43:55 -0700

    finish lesson03 - Create detail view for the category view, designed collection view for products

---
+++ CoderSwag/View/ProductCell.swift
@@ -0,0 +1,16 @@
+//
+//  ProductCell.swift
+//  CoderSwag
+//
+//  Created by David Sadler on 1/13/19.
+//  Copyright © 2019 David Sadler. All rights reserved.
+//
+
+import UIKit
+
+class ProductCell: UICollectionViewCell {
+    @IBOutlet weak var productImage: UIImageView!
+    @IBOutlet weak var productTitle: UILabel!
+    @IBOutlet weak var productPrice: UILabel!
+    
+}
